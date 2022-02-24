9d23a119c3969391fbae42d74ff2919ee1dd73ef
rav_official <97645551+ravoffc@users.noreply.github.com>
2022-02-24 16:46:35 +0700

    Add files via upload

---
+++ dist/_nuxt/d5bc758.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{286:function(t,e,n){"use strict";n.r(e);var c=n(63),component=Object(c.a)({},(function(){var t=this,e=t.$createElement;return(t._self._c||e)("h2",{staticClass:"text-center"},[t._v("BEST PRODUCT")])}),[],!1,null,null,null);e.default=component.exports}}]);
